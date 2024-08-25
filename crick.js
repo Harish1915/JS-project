fe34b4e7de7d98f6d1f1920d91e28536edc1740e
kasuba harish <kasubaharish04@gmail.com>
2024-08-25 16:58:51 +0530

    JS project cricket

---
+++ crick.js
@@ -0,0 +1,25 @@
+
+
+var div = document.getElementsByClassName("content");
+
+  var btn =document.getElementsByClassName("opn");
+
+  btn[0].addEventListener("click",function(){
+    
+    window.location.assign("./open.html");   
+  
+  });
+
+  // document.body.append(div[2]);
+
+ var ul= document.getElementsByClassName("nav");
+
+  ul[0].addEventListener("click",function(){
+
+    window.location.assign("./crickt.html");
+
+ });
+
+ 
+
+
